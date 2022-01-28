edcda82be95c9c9754f0577e71d355d1af66f19a
Davey-M <davey.meuer@gmail.com>
2022-01-28 15:40:49 -0600

    Updating total monthly costs on DOM

--- scripts/client.js
+++ scripts/client.js
@@ -9,10 +9,10 @@ function main() {
 
 let totalMonthlySalary = 0;
 let employeeIDs = [];
-let employees = [];
+let employees = {};
 
 class Employee {
-    
+
     constructor(firstName, lastName, idNumber, jobTitle, annualSalary) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -20,13 +20,12 @@ class Employee {
         this.jobTitle = jobTitle;
         this.annualSalary = annualSalary;
         this.monthlySalary = this.annualSalary / 12;
-        totalMonthlySalary += this.monthlySalary;
-        employeeIDs.push(this.idNumber);
-    }
 
-    delete() {
-        totalMonthlySalary -= this.monthlySalary;
-        $(`#${this.idNumber}`).remove();
+        totalMonthlySalary += this.monthlySalary;
+
+        employeeIDs.push(this.idNumber);
+
+        employees[this.idNumber] = this;
     }
 
     appendInfo() {
@@ -38,11 +37,19 @@ class Employee {
                 <td>${this.jobTitle}</td>
                 <td>${this.annualSalary}</td>
                 <td>
-                    <button class="deleteButton" onclick="${this.delete}" >Delete</button>
+                    <button class="deleteButton" deleteTarget="${this.idNumber}" >Delete</button>
                 </td>
             </tr>
         `);
+
+        updateMonthlyCosts();
     }
+}
+
+function removeRow(id) {
+    totalMonthlySalary -= employees[id].monthlySalary;
+    $(`#${id}`).remove();
+    updateMonthlyCosts();
 }
 
 function handleFormSubmit(e) {
@@ -54,9 +61,18 @@ function handleFormSubmit(e) {
         inputs.firstName.value, 
         inputs.lastName.value, 
         inputs.idNumber.value, 
-        inputs.firstName, 
-        inputs.firstName
+        inputs.jobTitle.value, 
+        inputs.annualSalary.value,
     );
+
+    newRow.appendInfo();
     
     console.log(e);
+}
+
+// Check for id already in use
+
+// Update the total monthly salary
+function updateMonthlyCosts() {
+    $(`#totalMonthlySalary`).text(totalMonthlySalary);
 }
