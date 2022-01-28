165a6e3fe1715ebd355db3936bfff8de44eeff52
Davey-M <davey.meuer@gmail.com>
2022-01-28 12:01:22 -0600

    WIP employee class

--- scripts/client.ts
+++ scripts/client.ts
@@ -7,14 +7,29 @@ function main() {
     $('#employeeForm').on('submit', handleFormSubmit);
 }
 
+let totalMonthlySalary = 0;
+let employeeIDs = [];
+
 class Employee {
+
+    public monthlySalary: number;
+
     constructor(
         public firstName: string,
         public lastName: string,
         public idNumber: number,
         public jobTitle: string,
         public annualSalary: number,
-    ) {};
+    ) {
+        this.monthlySalary = this.annualSalary / 12;
+        totalMonthlySalary += this.monthlySalary;
+
+        employeeIDs.push(this.idNumber);
+    };
+
+    delete() {
+
+    }
 }
 
 function handleFormSubmit(e: any) {
